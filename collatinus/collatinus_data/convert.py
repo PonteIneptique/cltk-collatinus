ac44c3a2e3b1abb66cc47878627b388a61e6decb
Thibault Clerice <leponteineptique@gmail.com>
2016-09-23 17:18:58 +0200

    Some more parsing

--- collatinus/collatinus_data/convert.py
+++ collatinus/collatinus_data/convert.py
@@ -1,9 +1,13 @@
 import json
 import re
 from copy import deepcopy
+import unicodedata
 
+
+#############################################################
 # Convert morphos.la
 # Each line of Morphos.la represents a declension name
+#############################################################
 morphs_name = {}
 with open("./collatinus/collatinus_data/src/morphos.la") as f:
     for index, line in enumerate(f.readlines()):
@@ -11,27 +15,51 @@ with open("./collatinus/collatinus_data/src/morphos.la") as f:
 
 assert morphs_name[190] == "vocatif masculin singulier participe présent actif"
 
+#############################################################
 # Convert modeles.la
 # Line starting with $ are variable that can be reused
 # Set of line starting with model are models
 # R:int:int,int (Root number, Character to remove to get canonical form, number of character to add to get the root)
 #  -> eg. : for uita, R:1:1,0 would get root 1, 1 character to remove, 0 to add -> uit
 #  -> eg. : for epulae, R:1:2,0 would get root 1, 2 character to remove, 0 to add : epul
-
-models = {}
-__model = {
-    "R": {},
-    "abs": [],  # Unused desinence if inherits
-    "des": {}
-}
-__R = re.compile("^R:(?P<root>\d+):(?P<remove>\w+)[,:]?(?P<add>\w+)?", flags=re.UNICODE)
-__des = re.compile("^des:(?P<range>[\d\-,]+):(?P<root>\d+):([\w\-,;]+)$", flags=re.UNICODE)
+#############################################################
 
 
-with open("./collatinus/collatinus_data/src/modeles.la") as f:
+def parse_range(des_number):
+    """ Range
+
+    :return: Int reprenting element of the range
+    """
+    ids = []
+    for des_group in des_number.split(","):  # When we have ";", we should parse it normally
+        if "-" in des_group:
+            start, end = tuple([int(x) for x in des_group.split("-")])
+            ids += list(range(start, end + 1))
+        else:
+            ids += [int(des_group)]
+    return ids
+
+
+def convert_models(lines, normalize=False):
+    models = {}
+    __model = {
+        "R": {},
+        "abs": [],  # Unused desinence if inherits
+        "des": {},  # Dict of desinences
+        "suf": {},  # Dict of Suffixes
+        "sufd": []  # Possible endings
+    }
+    __R = re.compile("^R:(?P<root>\d+):(?P<remove>\w+)[,:]?(?P<add>\w+)?", flags=re.UNICODE)
+    __des = re.compile("^des[\+]?:(?P<range>[\d\-,]+):(?P<root>\d+):([\w\-,;]+)$", flags=re.UNICODE)
     last_model = None
     variable_replacement = {}
-    for line in f.readlines():
+
+    if normalize:
+        lines = "\n".join(lines)
+        lines = unicodedata.normalize('NFKD', lines).encode('ASCII', 'ignore').decode()
+        lines = lines.split("\n")
+
+    for lineno, line in enumerate(lines):
         line = line.strip()
         # If we get a variable
         if line.startswith("$"):
@@ -46,13 +74,13 @@ with open("./collatinus/collatinus_data/src/modeles.la") as f:
             elif line.startswith("pere:"):
                 # Inherits from parent
                 models[last_model].update(
-                    models[line[5:]]
+                    deepcopy(models[line[5:]])
                 )
             elif line.startswith("R:"):
                 # Still do not know how to deal with "K"
                 root, remove, chars = __R.match(line).groups()
                 models[last_model]["R"][root] = [remove, chars]
-            elif line.startswith("des:"):
+            elif line.startswith("des"):
                 # We have new endings
                 # des:range:root_number:list_of_des
                 # First we apply desinence variables replacement
@@ -69,20 +97,47 @@ with open("./collatinus/collatinus_data/src/modeles.la") as f:
                         line = line.replace(var, rep)
                         if "$" not in line:
                             break
-                if not __des.match(line):
-                    print(line)
                 des_number, root, des = __des.match(line).groups()
 
-                ids = []
-                for des_group in des_number.split(","):  # When we have ";", we should parse it normally
-                    if "-" in des_group:
-                        start, end = tuple([int(x) for x in des_group.split("-")])
-                        ids += list(range(start, end+1))
-                    else:
-                        ids += [int(des_group)]
+                ids = parse_range(des_number)
                 for i, d in zip(ids, des.split(";")):
-                    models[last_model]["des"][i] = d.split(",")
+                    models[last_model]["des"][i] = (root, d.replace("-", "").split(","))
+            elif line.startswith("abs:"):
+                models[last_model]["abs"] = parse_range(line[4:])  # Add the one we should not find as desi
+            elif line.startswith("suf:"):
+                rng, suf = tuple(line[4:].split(":"))
+                models[last_model]["suf"] = {i: suf for i in parse_range(rng)}  # Sufd are suffix always present
+            elif line.startswith("sufd:"):
+                models[last_model]["sufd"] = line[5:]  # Sufd are suffix always present. Can has alternative
+            else:
+                print(line.split(":")[0])
+    return models
 
-    print(models["uita"])
-    print(models["epulae"])
-    print(models["fortis"])
+
+with open("./collatinus/collatinus_data/src/modeles.la") as f:
+    lines = f.read().split("\n")
+    models = convert_models(lines)
+    norm_models = convert_models(lines, True)
+
+
+assert models["fortis"]["des"][13] == ("4", ['']),\
+    "Root 4, Empty string (originally '-'), found %s %s" % models["fortis"]["des"][13]
+assert models["fortis"]["des"][51] == ("1", ["ĭōrĕm"]),\
+    "Root 4, Empty string (originally '-') found %s %s" % models["fortis"]["des"][50]
+
+assert norm_models["fortis"]["des"][13] == ("4", ['']),\
+    "Root 4, Empty string (originally '-'), found %s %s" % norm_models["fortis"]["des"][13]
+assert norm_models["fortis"]["des"][51] == ("1", ["iorem"]),\
+    "Root 4, Empty string (originally '-') found %s %s" % norm_models["fortis"]["des"][50]
+
+with open("./collatinus/collatinus_data/collected.json", "w") as f:
+    json.dump(
+        {
+            "morph-name": morphs_name,
+            "scansions": {
+                "models": models
+            },
+            "models": norm_models
+        },
+        f
+    )
